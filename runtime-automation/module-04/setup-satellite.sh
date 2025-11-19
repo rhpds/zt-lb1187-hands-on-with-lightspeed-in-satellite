542c01fdc803e25b12949c90556678fc96092fc7
Matthew Yee <myee@redhat.com>
2025-11-19 10:52:18 -0800

    Add host registration commands for RHEL10 in setup-satellite.sh
    This commit introduces commands to generate host registration scripts for RHEL10, enabling automated registration of hosts with specified activation keys and insights setup. The changes enhance the setup process by allowing seamless SSH execution of the registration commands for multiple hosts.

--- runtime-automation/module-04/setup-satellite.sh
+++ runtime-automation/module-04/setup-satellite.sh
@@ -24,3 +24,9 @@ tee ~/config.yml << EOF
 EOF
 
 ansible-playbook config.yml
+
+export regscript=$(hammer host-registration generate-command --activation-key RHEL10 --setup-insights 0 --insecure 1 --force 1)
+ssh -o "StrictHostKeyChecking no" root@rhel1 $regscript
+
+export regscript=$(hammer host-registration generate-command --activation-key RHEL10 --setup-insights 0 --insecure 1 --force 1)
+ssh -o "StrictHostKeyChecking no" root@rhel2 $regscript
