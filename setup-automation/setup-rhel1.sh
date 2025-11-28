4290b3a71be50c2b70ad169d560d6eb4c02a4b91
Matthew Yee <myee@redhat.com>
2025-11-28 08:40:41 -0800

    Stop, disable, and mask dnf-automatic-install.timer in setup scripts for RHEL1 and RHEL2

--- setup-automation/setup-rhel1.sh
+++ setup-automation/setup-rhel1.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+systemctl stop dnf-automatic-install.timer
+systemctl disable dnf-automatic-install.timer
+systemctl mask dnf-automatic-install.timer
 dnf install -y libvpx-1.14.1-2.el10
