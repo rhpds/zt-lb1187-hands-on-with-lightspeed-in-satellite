f43c33581fe09ecbc1f0dd1fb6af410b7e8328eb
Matthew Yee <myee@redhat.com>
2025-11-28 09:09:24 -0800

    Add setup scripts for RHEL1 and RHEL2 to install libvpx
    This commit introduces two new scripts, setup-rhel1.sh and setup-rhel2.sh, which automate the installation of the libvpx library on RHEL systems. Additionally, it removes the installation command from the existing setup scripts to streamline the automation process.

---
+++ runtime-automation/module-01/setup-rhel2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dnf install -y libvpx-1.14.1-2.el10
--- setup-automation/setup-rhel2.sh
+++ setup-automation/setup-rhel2.sh
@@ -2,4 +2,3 @@
 systemctl stop dnf-automatic-install.timer
 systemctl disable dnf-automatic-install.timer
 systemctl mask dnf-automatic-install.timer
-dnf install -y libvpx-1.14.1-2.el10
